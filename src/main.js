f638b318155aff30d04ace3ac52381a8a0ff0ab7
drfgjko <3091789938@qq.com>
2025-06-14 10:33:02 +0800

    init:mock配置

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/mock/index' // 必须在 main.js 顶部引入
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import pinia from '@/stores/index'
+
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteMockServe } from 'vite-plugin-mock'
+import path from 'path'
 
+const srcPath = path.resolve(__dirname, 'src')
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     viteMockServe({
-      mockPath: './mock',
+      mockPath: path.resolve(__dirname, 'src/mock'), //不能使用别名
       localEnable: true,
     }),
   ],
@@ -17,5 +19,11 @@ export default defineConfig({
         additionalData: `@use "@/styles/element.scss" as *;`,
       },
     },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.json', '.vue'],
   },
 })
